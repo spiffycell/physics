6c5086ff7dad2beb590936531bd8c64066827b60
spiffycell <spiffycell@protonmail.com>
2023-11-22 22:29:14 -0500

    add docstrings

--- die_system.py
+++ die_system.py
@@ -7,17 +7,17 @@ class LawOfStasis(LawOfMotion):
     example: sigma(t+1) = sigma(t)
     """
     def apply(self, current_state: State) -> State:
-        if current_state is current_state:
+        if current_state:
             return current_state
 
 class LawOfCycle(LawOfMotion):
     """
     Law of Motion: Over time, the State transitions from one to the next in ordered sequence.
     """
-    def apply(self, state_set: list, current_state: State) -> State:
-        for index in range(0, len(state_set)):
-            if state_set[index] == current_state:
-                new_state = state_set[(index + 1) % 6]
+    def apply(self, state_space: list, current_state: State) -> State:
+        for index in range(0, len(state_space)):
+            if state_space[index] == current_state:
+                new_state = state_space[(index + 1) % 6]
                 return new_state
 
 class LawOfConservedQuantity(LawOfMotion):
@@ -28,26 +28,34 @@ class LawOfConservedQuantity(LawOfMotion):
     Systems subject to this law can be said to have a Conserved Quantity
     In this example, if i set the current_state and give it a conserved quantity of 0, it will transition into itself
     if i set to a conserved quantity of 1, it will transition to one other state, then back to itself.
+
+    @param state_space          the set of possible states for a given system 
+    @param current_state        the current state or configuration of the given system      
+    @param conserved_quantity   the conserved quantity of the system. size of an iterative cycle, zero-indexed. 
+    @param position             the current_state position relative to the conserved_quantity 
+    @see
     """
-    def apply(self, state_set: list, current_state: State, conserved_quantity: int, position: int) -> State:
-        for index in range(0, len(state_set)):
+    def apply(self, state_space: list, current_state: State, conserved_quantity: int, position: int) -> State:
+        for index in range(0, len(state_space)):
             # find where in the array the current state is
-            if state_set[index] == current_state:
+            if state_space[index] == current_state:
                 # if the position of the state isn't at the end of the cycle 
                 if position < conserved_quantity:
                     # keep moving forward in the cycle
-                    return state_set[index + 1]
+                    return state_space[index + 1]
                 # else, if it's at the end of the cycle
                 elif position == conserved_quantity:
                     # circle back to the beginning of the cycle
-                    return state_set[index - conserved_quantity]
+                    # note that the conserved_quantity is equal to the distance 
+                    # between the first and final indices of the cycle
+                    return state_space[index - conserved_quantity]
 
 class LawOfRoll(LawOfMotion):
     """
     Law of Motion: Over time, the State transitions from one to one of six states (including the current state).
     """
-    def apply(self, state_set: list) -> State:
-        new_state = random.choice(state_set)
+    def apply(self, state_space: list) -> State:
+        new_state = random.choice(state_space)
         return new_state
 
 class DieSystem(System):
@@ -55,11 +63,11 @@ class DieSystem(System):
     def __init__(self):
         super().__init__(
             system_name = "Die System",
-            state_set = [State(state_name='One'), State(state_name='Two'), State(state_name='Three'), \
+            state_space = [State(state_name='One'), State(state_name='Two'), State(state_name='Three'), \
                     State(state_name='Four'), State(state_name='Five'), State(state_name='Six')],
             initial_condition = State(state_name='One'),
             current_state = State(state_name='One'),
             laws_of_motion = [LawOfStasis(), LawOfCycle(), LawOfConservedQuantity(), LawOfRoll()]
         )
-        self.initial_condition = random.choice(self.state_set)
+        self.initial_condition = random.choice(self.state_space)
         self.current_state = self.initial_condition
