c45558d1f32ab574b5295d2430d01f1ba7baaeca
spiffycell <spiffycell@protonmail.com>
2023-11-22 22:42:19 -0500

    fix docstrings for laws

--- laws.py
+++ laws.py
@@ -4,25 +4,43 @@ class LawOfStasis(LawOfMotion):
     Law of Motion: Over time, the State stays the same.
 
     example: sigma(t+1) = sigma(t)
+
+    @param  current_state   The current state or configuration of the given system      
+    @return new_state       The new state or configuration of the system after the law is applied
+    @see
     """
     def apply(self, current_state: State) -> State:
-        if current_state is current_state:
-            return current_state
+        if current_state:
+            new_state = current_state
+            return new_state
 
 class LawOfFlip(LawOfMotion):
     """
     Law of Motion: Over time, the State transitions for state set of size two.
 
     example: sigma(t+1) = -(sigma(t)) 
+
+    @param  state_space     The set of possible states for a given system 
+    @param  current_state   The current state or configuration of the given system      
+    @return new_state       The new state or configuration of the system after the law is applied
+    @see
     """
     def apply(self, state_space: list, current_state: State) -> State:
         if state_space[0] is current_state:
-            return state_space[1]
-        return state_space[0]
+            new_space = state_space[1]
+            return new_space
+        new_space = state_space[0]
+        return new_space
 
 class LawOfCycle(LawOfMotion):
     """
     Law of Motion: Over time, the State transitions from one to the next in ordered sequence.
+
+    @param  state_space     The set of possible states for a given system 
+    @param  current_state   The current state or configuration of the given system      
+    @return new_state       The new state or configuration of the system after the law is applied
+    @return
+    @see
     """
     def apply(self, state_space: list, current_state: State) -> State:
         for index in range(0, len(state_space)):
@@ -39,11 +57,11 @@ class LawOfConservedQuantity(LawOfMotion):
     In this example, if i set the current_state and give it a conserved quantity of 0, it will transition into itself
     if i set to a conserved quantity of 1, it will transition to one other state, then back to itself.
 
-    @param state_space          the set of possible states for a given system 
-    @param current_state        the current state or configuration of the given system      
-    @param conserved_quantity   the conserved quantity of the system. size of an iterative cycle, zero-indexed. 
-    @param position             the current_state position relative to the conserved_quantity 
-    @return State               the state that is transitioned to as a result of applying the law
+    @param state_space          The set of possible states for a given system 
+    @param current_state        The current state or configuration of the given system      
+    @param conserved_quantity   The conserved quantity of the system. size of an iterative cycle, zero-indexed. 
+    @param position             The current_state position relative to the conserved_quantity 
+    @return State               The state that is transitioned to as a result of applying the law
     @see
     """
     def apply(self, state_space: list, current_state: State, conserved_quantity: int, position: int) -> State:
@@ -64,6 +82,10 @@ class LawOfConservedQuantity(LawOfMotion):
 class LawOfRoll(LawOfMotion):
     """
     Law of Motion: Over time, the State transitions from one to one of six states (including the current state).
+
+    @param  state_space The set of all possible states for a given system
+    @return new_state   The new state or configuration of the system after the law is applied
+    @see
     """
     def apply(self, state_space: list) -> State:
         new_state = random.choice(state_space)
